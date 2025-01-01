773030ef180467f6272ca2a4e15c869e799b6960
spectagency <account@spectagency.com>
2025-01-01 13:12:14 +0100

    Update gsap-interactions.js

--- gsap-interactions.js
+++ gsap-interactions.js
@@ -1,5 +1,6 @@
-//select all buttons
+// Select all buttons
 const buttons = document.querySelectorAll('.primary-button');
+
 buttons.forEach((button) => {
   const iconWrapper = button.querySelector('.primary-button_icon-wrapper');
 
@@ -16,15 +17,9 @@ buttons.forEach((button) => {
 
     // Icon wrapper scale down during .3 seconds
     timeline.to(iconWrapper, {
-        scale: 0, // Scale down to 0
-        duration: 0.3,
-        ease: 'power1.out',
-      });
-
-    //instant opacity to 0 after scaling down
-    timeline.set(iconWrapper, {
-      opacity: 0, 
-      duration: 0,
+      scale: 0, // Scale down to 0
+      duration: 0.3,
+      ease: 'power1.out',
     });
 
     // Padding animation for the button
@@ -35,30 +30,29 @@ buttons.forEach((button) => {
         paddingRight: '2.625rem',
         duration: 0.3, // 0.3 seconds
         ease: 'power1.inOut',
-      },
+      }
     );
 
     // Update .primary-button_icon-wrapper position instantly
-    timeline.set(iconWrapper, {
+    timeline.to(iconWrapper, {
       right: '0.25rem', // Using rem explicitly
       left: 'auto',
-    }
-    );
+      duration: 0, // Instant update but respects the timeline
+    });
 
-    timeline.set(iconWrapper, {
+    // Reset opacity and scale before final scale-up
+    timeline.to(iconWrapper, {
       opacity: 1,
-      scale: 0,
+      scale: 0, // Reset scale to 0
+      duration: 0, // Instant but respects the timeline
     });
 
     // Final scale-up closer to padding change
-    timeline.to(
-      iconWrapper,
-      {
-        scale: 1.1, // Scale to 1.1
-        duration: 0.3,
-        ease: 'power1.in',
-      }
-    );
+    timeline.to(iconWrapper, {
+      scale: 1.1, // Scale to 1.1
+      duration: 0.3,
+      ease: 'power1.in',
+    });
 
     // Scale back to 1
     timeline.to(iconWrapper, {
